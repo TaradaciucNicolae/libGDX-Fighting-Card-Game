82d8132435e4c01a25085bcf9a0b390b1de38f07
Vicol Serban-Ilie <serban-ilie.vicol@student.tuiasi.ro>
2023-04-27 14:58:33 +0300

    Animation test

--- GDX Fighting Card Game Project/core/src/com/gdx/game/FightScreen.java
+++ GDX Fighting Card Game Project/core/src/com/gdx/game/FightScreen.java
@@ -3,16 +3,29 @@ package com.gdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class FightScreen implements Screen {
 	private GdxFightingGame game;
 	private Stage stage;
+	
+	//Player idle animation test
+	Animation<TextureRegion> playeridle;
+	Texture idlesheet;
+	SpriteBatch spriteBatch;
+	float stateTime;
+	//
+	
 	
 	public FightScreen(GdxFightingGame agame) {
 		// TODO Auto-generated constructor stub
@@ -21,6 +34,21 @@ public class FightScreen implements Screen {
 		
 		final Monster m1=new Monster();
 		Player p=new Player();
+		//animation
+		idlesheet=new Texture(Gdx.files.internal("Player//KnightIdle_strip.png"));
+		TextureRegion[][] tmp = TextureRegion.split(idlesheet,idlesheet.getWidth()/15,idlesheet.getHeight());
+		//15 is the number of frames and colums we have
+		TextureRegion[] Player_frames= new TextureRegion[15];
+		int index=0;
+		for(int i=0;i<15;++i) {
+			Player_frames[index++]=tmp[0][i];
+		}
+		
+		playeridle=new Animation<TextureRegion>(0.025f,Player_frames);
+		spriteBatch=new SpriteBatch();
+		stateTime=0f;
+		 //
+		
 		Table table=new Table();
 		TextButton button = new TextButton("attack", GdxFightingGame.gameSkin);
 		table.add(button);
@@ -52,8 +80,19 @@ public class FightScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //animation
+        stateTime += Gdx.graphics.getDeltaTime();
+        //
         stage.act();
         stage.draw();
+        //animation
+        TextureRegion currentFrame = playeridle.getKeyFrame(stateTime, true);
+		spriteBatch.begin();
+		spriteBatch.draw(currentFrame, 200, 100,192,128); // Draw current frame at (50, 50)
+		spriteBatch.end();
+        //
+		
+        
     }
  
     @Override
@@ -78,7 +117,12 @@ public class FightScreen implements Screen {
  
     @Override
     public void dispose() {
-   stage.dispose();
-  }
+    //animation
+    // SpriteBatches and Textures must always be disposed
+    spriteBatch.dispose();
+    idlesheet.dispose();
+    //
+    stage.dispose();
+    }
 
 }
--- GDX Fighting Card Game Project/core/src/com/gdx/game/GdxFightingGame.java
+++ GDX Fighting Card Game Project/core/src/com/gdx/game/GdxFightingGame.java
@@ -4,6 +4,10 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -17,11 +21,11 @@ public class GdxFightingGame extends Game {
 
 	private Game game;
 	
-	
 	 static public Skin gameSkin;
 	  public void create () {
 	    gameSkin = new Skin(Gdx.files.internal("glassy//skin//glassy-ui.json"));
 	    this.setScreen(new MainScreen(this));
+	    
 	  }
 	 
 	

9dae53890394e024427f281bd30e734757eaa3aa
axel_alex <alexandru.munteanu@student.tuiasi.ro>
2023-05-18 13:59:19 +0300

    junit test

---
+++ GDX Fighting Card Game Project/core/src/com/gdx/game/MonsterTest.java
@@ -0,0 +1,49 @@
+package com.gdx.game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MonsterTest {
+
+	@Test
+	void testMonster() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetHealth() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetDamage() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetMove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testHeal() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testMoves() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSetArmour() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSetHealth() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
